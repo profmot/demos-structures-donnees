4a5902a0d92704fb79635d5db3a60c5f9e22390b
Prof MOT <mo.tremblay@cmontmorency.qc.ca>
2024-11-03 16:34:33 -0500

    listeNaiveGr2

--- README.md
+++ README.md
@@ -1,15 +1,38 @@
-# 3c6_exemples
 
-depot cloné de https://gitlab.com/mathieu-bergeron/3c6_exemples fait par Mathieu Bergeron.
+public static int rechercheBinaireRecursive(int[] tableau, int valeur, int debut, int fin) {
 
-1. Pour créer les projets Eclipse
 
-    ```bash
-    $ sh gradlew eclipse --continue
-    ```
+    {1:MCS:=if (debut > fin) &#123~while (debut <= fin) &#123~∅}
 
-1. Pour exécuter un projet, p.ex. `demo1_2_gr1`
+        {1:MCS:=return -1;~int milieu = (debut + fin) / 2;~∅}
 
-    ```bash
-    $ sh gradlew demo1_2
-    ```
+    {1:MCS:=&#125;~∅}
+
+    {1:MCS:=int milieu = (debut + fin) / 2;~∅}
+
+    {1:MCS:=if (tableau[milieu] == valeur) &#123;~∅}
+
+        {1:MCS:=return milieu;~∅}
+
+    {1:MCS:=&#125;~∅}
+
+
+    {1:MCS:=if (valeur < tableau[milieu]) &#123;~∅}
+
+        {1:MCS:=return rechercheBinaireRecursive(tableau, valeur, debut, milieu - 1);~fin = milieu - 1;~∅}
+
+    {1:MCS:=&#125 else &#123;~&#123;~∅}
+
+        {1:MCS:=return rechercheBinaireRecursive(tableau, valeur, milieu + 1, fin);~debut = milieu + 1;~∅}  
+
+    {1:MCS:=&#125;~&#125 else &#123;~∅}
+
+    {1:MCS:=∅~&#125 else &#123}
+
+    {1:MCS:=∅~return -1;}
+
+}
+
+
+
+
--- demo4_1/src/main/java/demo4_1/ListeNaive.java
+++ demo4_1/src/main/java/demo4_1/ListeNaive.java
@@ -8,48 +8,41 @@ public class ListeNaive extends Modele<ListeNaive> {
     
 	private char[] valeurs = new char[0];
     
-    
 
     @Override
     public void initialize() {
         
-        this.add('a');
-        this.add('b');
-        this.add('c');
-        this.add('d');
-        this.add('e');
-        this.add('f');
-        int i = this.indexOf('d');
-        System.out.println(i);
+       
+    }
 
-		int taille = this.size();
-        System.out.println("taille");
-		System.out.println(taille);
-        this.removeIndex(2);
-        this.removeValue('d');
-        taille = this.size();
-		System.out.println("taille après remove");
-		System.out.println(taille);
+    public void set(int position, char valeur){
+        this.valeurs[position]=valeur;
+    }
 
-        this.addAll(new char[] {'5','d','c','f'});
-		taille = this.size();
-		System.out.println("taille addAll");
-		System.out.println(taille);
+    public char get(int position){
+        return this.valeurs[position];
     }
 
 	private void add(char c) {
         // créer un nouveau tableau de char de longueur + 1
+        char[] nouveauTableau = new char[valeurs.length+1];
         
         // boucle qui ajoute tous les éléments déjà présent dans le tableau valeurs
-        
+        for(int i =0; i<valeurs.length;i++){
+            nouveauTableau[i] = valeurs[i];
+        }
     
         // ajout du nouvel élément à la dernière position du tableau
-        
+        nouveauTableau[nouveauTableau.length -1] = c; 
         //assigner valeur du nouveau tableau
+        this.valeurs = nouveauTableau;
     }
 
     private void addAll(char[] cs) {
         //boucle et ajoute chaque élément
+        for(int i =0 ; i<cs.length;i++){
+            add(cs[i]);
+        }
     }
 
     private int indexOf(char c) {
@@ -57,29 +50,39 @@ public class ListeNaive extends Modele<ListeNaive> {
         //boucle tous les elements de valeurs
             //si l'element à la position indice i = c
                 //conserver l'indice i et arrêter
+        for(int i =0; i<valeurs.length; i++){
+            if( valeurs[i] == c){
+                indiceDe = i;
+                break;
+            }
+        }
         return indiceDe;
     }
 
     private void removeIndex(int indice) {
         //créer un nouveau tableau d'une taille plus petite que longueur de valeur
-
-
-        //boucler tous les éléments de valeur
-            // si le i < indice, transférer la valeur dans le nouveau tableau à la même position
-            
-            // sinon si i > indice, transférer la valeur dans le nouveau tableau avec une position de moins
-                
+        char[] nouveauTableau = new char[valeurs.length-1];
+        for( int i = 0; i< valeurs.length;i++){
+            if (i< indice){
+                nouveauTableau[i] = valeurs[i];
+            } else if( i>indice){
+                nouveauTableau[i-1] = valeurs[i];
+            }
+        }
         //assigner valeurs du nouveau tableau
+        valeurs = nouveauTableau;
     }
 
 	public int size(){
-		return 0;
+		return valeurs.length;
 	}
 
     
     private void removeValue(char c) {
-        // trouver le c à l'aide de la methode indexOf(c)
-        //si trouvé ( indice >=0) enlever l'élément avec removeIndex
+       int indiceAEnlever= indexOf(c);
+       if(indiceAEnlever>=0 ){
+        removeIndex(indiceAEnlever);
+       }
     }
 
     public static void main(String[] args) {
