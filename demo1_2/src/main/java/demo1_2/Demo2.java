e5c1fb63d6398d398f4ee518c4440f85c644459a
Prof MOT <mo.tremblay@cmontmorency.qc.ca>
2025-08-27 11:16:53 -0400

    MOT: DemoHeritage et polymorphisme

--- demo1_2/src/main/java/demo1_2/Demo2.java
+++ demo1_2/src/main/java/demo1_2/Demo2.java
@@ -1,5 +1,7 @@
 package demo1_2;
 
+import java.util.List;
+
 import demo1_2.validateur.Modele;
 
 public class Demo2 extends Modele<Demo2> {
@@ -26,6 +28,42 @@ public class Demo2 extends Modele<Demo2> {
         personnages[0].subirDegats(30);
         personnages[1].subirDegats(50);
 
+
+/*      // Upcasting implicite: sous-classes vues comme Personnage
+        List<Personnage> equipe = List.of(
+            new Guerrier("Arthas"),
+            new Mage("Jaina")
+        );
+
+        Mission mission = new MissionCombat();
+
+        // POLYMORPHISME: le même code appelle la "bonne" implémentation
+        for (Personnage p : equipe) {
+            mission.executer(p);
+        }
+
+        // instanceof (pattern matching) + transtypage sécurisé
+        Personnage a = equipe.get(0); // Guerrier
+        if (a instanceof Guerrier g) {
+            g.coupPuissant(); // OK, accès à l’API Guerrier
+        }
+
+        Personnage b = equipe.get(1); // Mage
+        if (b instanceof Mage m) {
+            m.lancerSort(); // OK, accès à l’API Mage
+        }
+
+        // Downcast "classique" (évitez sans instanceof)
+        Personnage x = new Guerrier("Thorin");
+        if (x instanceof Guerrier) {
+            Guerrier gx = (Guerrier) x; // cast explicite
+            gx.coupPuissant();
+        }
+
+        // Mauvais cast (exemple à NE PAS faire) : ClassCastException
+        // Guerrier gg = (Guerrier) new Mage("FauxGuerrier"); // NE PAS FAIRE
+    }
+ */
 	}
 
 }
---
+++ demo1_2/src/main/java/demo1_2/MissionCombat.java
@@ -0,0 +1,27 @@
+package demo1_2;
+
+class MissionCombat extends Mission {
+    public MissionCombat(String nomMission) {
+        super(nomMission);
+    }
+
+    @Override
+    protected void preparer(Personnage p) {
+        System.out.println("Le héros équipe son épée.");
+    }
+
+    @Override
+    protected void demarrer(Personnage p) {
+        System.out.println("Le combat de " + p.getNom()+ " commence !");
+    }
+
+    @Override
+    protected void terminer(Personnage p) {
+        System.out.println("Le héros"+p.getNom()+" a vaincu ses ennemis !");
+    }
+
+    @Override
+    public void afficherNomMission() {
+        System.out.println("Mission de Combat: " + getNomMission());
+    }
+}
