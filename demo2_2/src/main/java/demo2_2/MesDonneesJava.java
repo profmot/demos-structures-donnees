f4e8fb66d7f117e3f66a111d5b971be169a49450
Prof MOT <mo.tremblay@cmontmorency.qc.ca>
2025-09-17 14:57:31 -0400

    MOT: demo 2.2

--- demo2_2/src/main/java/demo2_2/MesDonneesJava.java
+++ demo2_2/src/main/java/demo2_2/MesDonneesJava.java
@@ -11,6 +11,10 @@ import demo2_2.validateur.Modele;
 
 public class MesDonneesJava extends Modele<MesDonneesJava> {
 
+	public enum Couleur {
+		ROUGE, VERT, BLEU
+	}
+
 	private Map<String, Object> racine;
 
 	public Map<String, Object> getRacine() {
@@ -24,7 +28,28 @@ public class MesDonneesJava extends Modele<MesDonneesJava> {
 	@Override
 	public void initialize() {
 
-		Map<String, Object> maRacine = new NtroHashMap();
+		Map<String, Object> maRacine = new HashMap();
+		GardeRobe maGardeRobe = new GardeRobe();
+		maGardeRobe.setEmplacement("chambre");
+
+		List<Object> monContenu = new ArrayList<>();
+		Map<String, Object> chapeau1 = new HashMap<>();
+		chapeau1.put("chaud", false);
+		chapeau1.put("contenudans", maGardeRobe);
+		chapeau1.put("type", "Paille");
+		chapeau1.put("tailleRebord", 12.45);
+
+		Map<String, Object> chapeau2 = new HashMap<>();
+		chapeau2.put("chaud", false);
+		chapeau2.put("contenudans", maGardeRobe);
+		chapeau2.put("type", "Tuque");
+		// chapeau2.get("contenudans");
+		monContenu.add(chapeau1);
+		monContenu.add(chapeau2);
+
+		maRacine.put("gardeRobe", maGardeRobe);
+		maGardeRobe.setContient(monContenu);
+		this.setRacine(maRacine);
 
 	}
 
