2293e0e681eff3e08ab6197b688fe0c5039fbad1
Prof MOT <mo.tremblay@cmontmorency.qc.ca>
2025-09-17 13:30:21 -0400

    MOT demo 22

--- demo2_2/src/main/java/demo2_2/MesDonneesJava.java
+++ demo2_2/src/main/java/demo2_2/MesDonneesJava.java
@@ -10,61 +10,22 @@ import demo2_2.validateur.Modele;
 @SuppressWarnings("serial")
 
 public class MesDonneesJava extends Modele<MesDonneesJava> {
-	
+
 	private Map<String, Object> racine;
-	
-	
-	
 
 	public Map<String, Object> getRacine() {
 		return racine;
 	}
 
-
-
-
 	public void setRacine(Map<String, Object> racine) {
 		this.racine = racine;
 	}
 
-
-
-
 	@Override
 	public void initialize() {
-		// Interface  ... new Implantation
-		// Liste      ... new MaListe
-		// Arbre      ... new MonArbre
-		
-		
-		Map<String, Object> maRacine = new NtroHashMap();
-		this.racine = maRacine;
-		
-		List<Object> items = new ArrayList<>();
-		
-		GardeRobe gardeRobe = new GardeRobe();
-		
-		Map<String, Object> chapeau01 = new NtroHashMap<>();
 
-		Map<String, Object> chapeau02 = new NtroHashMap<>();
-		
-		this.racine = maRacine;
-		
-		this.racine.put("gardeRobe", gardeRobe);
-		
-		items.add(chapeau01);
-		items.add(chapeau02);
-		
-		gardeRobe.setContient(items);
-		
+		Map<String, Object> maRacine = new NtroHashMap();
+
 	}
 
 }
-
-
-
-
-
-
-
-
